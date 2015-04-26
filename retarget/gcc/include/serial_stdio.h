b2b02d881087ea35e28d5a4262d0d5e13d434533
Manuel Limones <limonesu.me@gmail.com>
2015-04-25 21:03:44 -0500

    fix macro definition at serial parameter

--- retarget/gcc/include/serial_stdio.h
+++ retarget/gcc/include/serial_stdio.h
@@ -15,7 +15,7 @@ int serial_gets(Serial_t  serial, char * pBuffer, int bufferSize);
 #define serial_printf(serial,...) {\
 	char * __serial_temp_buffer;\
 	asprintf(&__serial_temp_buffer, __VA_ARGS__);\
-	serial_puts(USART0_serial,__serial_temp_buffer);\
+	serial_puts(serial,__serial_temp_buffer);\
 	free(__serial_temp_buffer);\
 }
 
