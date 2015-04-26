95098b81721bf31d9653c9794e941e06aee916b2
Manuel Limones <limonesu.me@gmail.com>
2015-04-25 19:28:11 -0500

    add retarget functions for UART3

--- retarget/gcc/include/retarget_tm4c.h
+++ retarget/gcc/include/retarget_tm4c.h
@@ -4,5 +4,10 @@
 void UART0_init(int baudrate);
 void UART0_sendChar(char c);
 char UART0_getChar(void);
+
+void UART3_init(int baudrate);
+void UART3_sendChar(char c);
+char UART3_getChar(void);
+
 #endif
 
