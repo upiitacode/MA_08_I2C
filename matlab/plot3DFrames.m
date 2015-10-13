bed0cdf5ab88d328f9bd0b68fcfdf7c57762e72a
Manuel Limones <limonesu.me@gmail.com>
2015-10-13 03:53:32 -0500

    Add function that plots 3d frames

---
+++ matlab/plot3DFrames.m
@@ -0,0 +1,20 @@
+function [ ] = plot3DFrames(base_frame, rotated_frame, axis_handle)
+%UNTITLED8 Summary of this function goes here
+%   Detailed explanation goes here
+origin = [0 0 0];
+axes(axis_handle);
+hold off
+quiver3(origin,origin,origin,base_frame(1,:),base_frame(2,:),base_frame(3,:));
+hold on
+az = 135;
+el = 36;
+view(az, el);
+axis_handle.XLim=[-1 1];
+axis_handle.YLim=[-1 1];
+axis_handle.ZLim=[-1 1];
+quiver3(origin,origin,origin,rotated_frame(1,:),rotated_frame(2,:),rotated_frame(3,:));
+axis on
+grid off
+hold off
+end
+
