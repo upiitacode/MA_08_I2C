f744c3785e38777a61219bfb7a9e8f4950ae0935
Manuel Limones <limonesu.me@gmail.com>
2015-10-21 05:55:33 -0500

    Add RTOS

--- source/main.cc
+++ source/main.cc
@@ -1,19 +1,22 @@
 #include "TM4C123.h"                    // Device header
 #include "serial_stream_tm4c.h"
 #include "RGBLed.h"
-
-void delay_ms(int delay);
+#include "cmsis_os.h"                   // ARM::CMSIS:RTOS:Keil RTX
 
 int main(){
 	//Iitialize system
 	SerialUSART0 serial(9600);
 	RGBLed led;
-	int myBuffer[20];
-
+	SystemCoreClockUpdate();
+	osKernelInitialize();
+	osKernelStart();
 	//User application
-	led.write(RGB_COLOR_RED);
-	serial.printf("Hello, World!\n");
+	led.write(RGB_COLOR_WHITE);
+	serial.printf("CPU speed: %d\n",(int) SystemCoreClock);
 	while(1){
-		led.write(serial.getChar());
+		osDelay(500);
+		led.write(RGB_COLOR_BLUE);
+		osDelay(500);
+		led.write(RGB_COLOR_GREEN);
 	}
 }
