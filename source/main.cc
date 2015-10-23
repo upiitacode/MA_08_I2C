8df17e799d26e84e0bc1d2b80230bbad39f44a8e
Manuel Limones <limonesu.me@gmail.com>
2015-10-23 03:36:09 -0500

    Working application with OS_Timer

--- source/main.cc
+++ source/main.cc
@@ -8,48 +8,72 @@
 #include "SquareIntegral.h"
 #include "Threshold.h"
 
+void Timer1_Callback  (void const *arg); 
+osTimerDef (Timer1, Timer1_Callback);
+
+RGBLed* led;
+MPU6050* mpu;
+RangeThreshold* threshold;
+Data3D<float>* gyroData;
+SquareIntegral* integralx;
+SquareIntegral* integraly;
+SquareIntegral* integralz;
+Data3D<float>* filteredData;
+
 int main(){
-	//Iitialize system
-	SerialUSART0 serial(9600);
-	RGBLed led;
+	//Initialize system
+	SerialUSART0 serial(38400);
+	led = new RGBLed;
 	I2CBus0 i2c;
-	MPU6050 mpu(i2c);
-	mpu.calibrateGyro();
+	mpu = new MPU6050(i2c);
+	mpu->calibrateGyro();
 
 	//User application
-	led.write(RGBLed::COLOR_WHITE);
+	led->write(RGBLed::COLOR_WHITE);
 	serial.printf("CPU speed: %d\n",(int) SystemCoreClock);
 
-	RangeThreshold threshold(-5,5);
-	Data3D<float> gyroData;
-	Data3D<float> filteredData;
+	threshold = new RangeThreshold(-4.0,4.0);
+	gyroData = new Data3D<float>;
+	filteredData = new Data3D<float>;
 
-	SquareIntegral integralx(0,0.1);
-	SquareIntegral integraly(0,0.1);
-	SquareIntegral integralz(0,0.1);
+	integralx = new SquareIntegral(0,0.005);
+	integraly = new SquareIntegral(0,0.005);
+	integralz = new SquareIntegral(0,0.005);
 
 	//OS Configuration
 	SystemCoreClockUpdate();
+	osTimerId id1;  
 	osKernelInitialize();
+	uint32_t exec1 = 1;
+  id1 = osTimerCreate (osTimer(Timer1), osTimerPeriodic, &exec1);
 	osKernelStart();
+	osTimerStart (id1, 5); 
 
 	while(1){
-		gyroData = mpu.readCompensatedGyro();
-		filteredData.x = threshold.filter(gyroData.x);
-		filteredData.y = threshold.filter(gyroData.y);
-		filteredData.z = threshold.filter(gyroData.z);
-
-		integralx.addDeltaX(filteredData.x);
-		integraly.addDeltaX(filteredData.y);
-		integralz.addDeltaX(filteredData.z);
-
-		serial.printf("Wx=%4.1f,Wy=%4.1f,Wz=%4.1f,Tx=%4.1f,Ty=%4.1f,Tz=%4.1f\n",
-				filteredData.x,
-				filteredData.y,
-				filteredData.z,
-				integralx.getValue(),
-				integraly.getValue(),
-				integralz.getValue());
-		delay_ms(100);
+		serial.printf("Tx=%4.1f,Ty=%4.1f,Tz=%4.1f\n",
+				integralx->getValue(),
+				integraly->getValue(),
+				integralz->getValue());
+		osDelay(100);
 	}
+}
+
+int lastOutput = 0;
+
+void Timer1_Callback  (void const *arg){
+	if(lastOutput == 1){
+		lastOutput = 0;
+		led->write(RGBLed::COLOR_BLUE);
+	}else{
+		lastOutput = 1;
+		led->write(RGBLed::COLOR_RED);
+	}
+	*gyroData = mpu->readCompensatedGyro();
+	filteredData->x = (2000.0/65535.0) * threshold->filter(gyroData->x);
+	filteredData->y = (2000.0/65535.0) * threshold->filter(gyroData->y);
+	filteredData->z = (2000.0/65535.0) * threshold->filter(gyroData->z);
+
+	integralx->addDeltaX(filteredData->x);
+	integraly->addDeltaX(filteredData->y);
+	integralz->addDeltaX(filteredData->z);
 }
