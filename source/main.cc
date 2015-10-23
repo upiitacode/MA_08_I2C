18d7a23554236cd3b2100faec1ba05206272c2d8
Manuel Limones <limonesu.me@gmail.com>
2015-10-23 01:09:06 -0500

    Add new main app

--- source/main.cc
+++ source/main.cc
@@ -5,6 +5,8 @@
 #include "delay.h"
 #include "I2CBus_TM4C.h"
 #include "MPU6050.h"
+#include "SquareIntegral.h"
+#include "Threshold.h"
 
 int main(){
 	//Iitialize system
@@ -12,27 +14,42 @@ int main(){
 	RGBLed led;
 	I2CBus0 i2c;
 	MPU6050 mpu(i2c);
+	mpu.calibrateGyro();
+
+	//User application
+	led.write(RGBLed::COLOR_WHITE);
+	serial.printf("CPU speed: %d\n",(int) SystemCoreClock);
+
+	RangeThreshold threshold(-5,5);
+	Data3D<float> gyroData;
+	Data3D<float> filteredData;
+
+	SquareIntegral integralx(0,0.1);
+	SquareIntegral integraly(0,0.1);
+	SquareIntegral integralz(0,0.1);
 
 	//OS Configuration
 	SystemCoreClockUpdate();
 	osKernelInitialize();
 	osKernelStart();
 
-	//User application
-	led.write(RGBLed::COLOR_WHITE);
-	serial.printf("CPU speed: %d\n",(int) SystemCoreClock);
-
 	while(1){
-		serial.printf("Ax = %d, Ay = %d, Az = %d, Wx = %d, Wy = %d, Wz = %d\n",
-			mpu.readAccel('x'),
-			mpu.readAccel('y'),
-			mpu.readAccel('z'),
-			mpu.readGyro('x'),
-			mpu.readGyro('y'),
-			mpu.readGyro('z'));
-		delay_us(500000);
-		led.write(RGBLed::COLOR_BLUE);
-		delay_us(500000);
-		led.write(RGBLed::COLOR_GREEN);
+		gyroData = mpu.readCompensatedGyro();
+		filteredData.x = threshold.filter(gyroData.x);
+		filteredData.y = threshold.filter(gyroData.y);
+		filteredData.z = threshold.filter(gyroData.z);
+
+		integralx.addDeltaX(filteredData.x);
+		integraly.addDeltaX(filteredData.y);
+		integralz.addDeltaX(filteredData.z);
+
+		serial.printf("Wx=%4.1f,Wy=%4.1f,Wz=%4.1f,Tx=%4.1f,Ty=%4.1f,Tz=%4.1f\n",
+				filteredData.x,
+				filteredData.y,
+				filteredData.z,
+				integralx.getValue(),
+				integraly.getValue(),
+				integralz.getValue());
+		delay_ms(100);
 	}
 }
