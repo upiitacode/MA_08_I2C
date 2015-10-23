40cadbc74a55181a1cb1627a973f99d3230e3515
Manuel Limones <limonesu.me@gmail.com>
2015-10-22 23:28:31 -0500

    Use MPU6050 in main.cc

--- source/main.cc
+++ source/main.cc
@@ -3,18 +3,33 @@
 #include "RGBLed.h"
 #include "cmsis_os.h"                   // ARM::CMSIS:RTOS:Keil RTX
 #include "delay.h"
+#include "I2CBus_TM4C.h"
+#include "MPU6050.h"
 
 int main(){
 	//Iitialize system
 	SerialUSART0 serial(9600);
 	RGBLed led;
+	I2CBus0 i2c;
+	MPU6050 mpu(i2c);
+
+	//OS Configuration
 	SystemCoreClockUpdate();
 	osKernelInitialize();
 	osKernelStart();
+
 	//User application
 	led.write(RGBLed::COLOR_WHITE);
 	serial.printf("CPU speed: %d\n",(int) SystemCoreClock);
+
 	while(1){
+		serial.printf("Ax = %d, Ay = %d, Az = %d, Wx = %d, Wy = %d, Wz = %d\n",
+			mpu.readAccel('x'),
+			mpu.readAccel('y'),
+			mpu.readAccel('z'),
+			mpu.readGyro('x'),
+			mpu.readGyro('y'),
+			mpu.readGyro('z'));
 		delay_us(500000);
 		led.write(RGBLed::COLOR_BLUE);
 		delay_us(500000);
